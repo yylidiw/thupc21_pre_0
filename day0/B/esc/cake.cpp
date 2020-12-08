806d60f41fda3e6f201b4ffb4790b29769b3c234
EliminateSpace <EliminateSpace@users.noreply.github.com>
2020-12-08 21:51:31 +0800

    Update esc/cake.cpp

--- day0/B/esc/cake.cpp
+++ day0/B/esc/cake.cpp
@@ -19,9 +19,10 @@ int g[2555][2555];
 int main()
 {
     int n=io::F(),m=io::F();
+    double sum=0;
     assert(1<=n&&n<=2500);
     assert(1<=m&&m<=n);
-    for(int i=1;i<=n;++i)x[i]=io::F(),assert(x[i]>=1&&x[i]<=50000);
+    for(int i=1;i<=n;++i)x[i]=io::F(),assert(x[i]>=1&&x[i]<=50000),sum+=x[i];
     std::sort(x+1,x+n+1);
     for(int i=n;i;--i)
         for(int j=m;j;--j)
@@ -30,6 +31,6 @@ int main()
             g[i][j]=f[i+1][j]>(x[i]+f[i+1][j]+f[i+1][j-1])/2;
             assert(!(g[i][j]==0&&g[i][j+1]==1));
         }
-    printf("%.10lf\n",f[1][m]);
+    printf("%.10lf\n",sum-f[1][m]);
     return 0;
 }
