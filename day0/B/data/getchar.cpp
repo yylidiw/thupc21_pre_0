f9f55a9bfed064082d4af4114bc2a788bdcb8d86
EliminateSpace <EliminateSpace@users.noreply.github.com>
2020-12-09 00:21:43 +0800

    Update data of C

--- day0/B/data/getchar.cpp
+++ day0/B/data/getchar.cpp
@@ -5,7 +5,7 @@ int main()
     int cn=0;
     while(ch=getchar(),1)
     {
-        if(ch>='0'&&ch<='9'||ch==' '||ch=='\n'||ch=='.'||ch=='-'||ch=='+')putchar(ch);
+        if(ch>='0'&&ch<='9'||ch==' '||ch=='\n'||ch=='.'||ch=='-'||ch=='+'||ch>='A'&&ch<='z')putchar(ch);
         //printf("%d\n",(int)ch);
         if(++cn==3000000)return 0;
     }
