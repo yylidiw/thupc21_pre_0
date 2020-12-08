c3e25ec10e397f0916ced4eaa6ce6f47fd775f15
EliminateSpace <EliminateSpace@users.noreply.github.com>
2020-12-08 21:56:40 +0800

    Increase precision

--- day0/B/esc/rn2.py
+++ day0/B/esc/rn2.py
@@ -1,4 +1,4 @@
 from os import system
 for i in range(1,31):
     fn = "checkout"+str(i)
-    system("cake.exe < ../data/"+fn+".in > ../data/tmp/"+fn+".ans")
+    system("cake.exe < ../data/"+fn+".in > ../data/"+fn+".ans")
